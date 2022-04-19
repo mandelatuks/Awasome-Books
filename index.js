605bb108aafcc07f102ef81c807a9a7003369bc8
mandelatuks <manderat206@gmail.com>
2022-04-19 19:58:26 +0200

    Creat class for awesome books

--- index.js
+++ index.js
@@ -1,63 +1,37 @@
-const books = [
-  {
-    title: 'Life and Living',
-    author: 'John Doe',
-    id: 1,
-  },
-  {
-    title: 'Becoming',
-    author: 'Michelle Obama',
-    id: 2,
-  },
-];
+const bookList = document.querySelector('.books');
+const form = document.getElementById('add-book');
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
 
-const bookListSection = document.querySelector('#book-list');
+let books = [];
 
-function displayBooksList(bookList) {
-  bookListSection.innerHTML = bookList.map((book) => `
-<p class="title">${book.title}</p>
-            <p>${book.author}</p>
-            <button data-id=${book.id} class="remove">Remove</button>
-            <hr>`).join('');
-}
-
-function storeBook(bookList) {
-  localStorage.setItem('bookList', JSON.stringify(bookList));
-}
-
-function getBookList() {
-  const bookListFromLocalStorage = localStorage.getItem('bookList');
-  if (bookListFromLocalStorage) {
-    return JSON.parse(bookListFromLocalStorage);
+class BookClass {
+  constructor(title, author, id) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
   }
-  return books;
+
+  bookCode() {
+    return `<p class="title">${this.title}</p>
+                 <p>${this.author}</p>
+                <button data-id=${this.id} class="remove">Remove</button>
+                <hr>`;
+  }
+
+  static addBook(book) {
+    let id = 1;
+    if (books.length > 0) {
+      id = books[books.length - 1].id + 1;
+    }
+    book.id = id;
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static remove(id) {
+    books = books.filter((b) => b.id !== Number(id));
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 }
 
-displayBooksList(getBookList());
-
-const addBook = document.querySelector('#add-book');
-addBook.addEventListener('submit', function (event) {
-  event.preventDefault();
-  const title = event.target.querySelector('#title').value;
-  const author = event.target.querySelector('#author').value;
-  const bookList = getBookList();
-  const id = bookList.length + 1;
-  bookList.push({
-    title,
-    author,
-    id,
-  });
-  this.reset();
-  displayBooksList(bookList);
-  storeBook(bookList);
-});
-
-bookListSection.addEventListener('click', (event) => {
-  if (event.target.classList.contains('remove')) {
-    const { id } = event.target.dataset;
-    const bookList = getBookList();
-    const bookListFiltered = bookList.filter((book) => book.id !== +id);
-    displayBooksList(bookListFiltered);
-    storeBook(bookListFiltered);
-  }
-});
